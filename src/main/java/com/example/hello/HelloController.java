108a0c41fb8373fb8be162d820da22c4bf697c67
NakamuraYuri0421 <nkmr44yr@gmail.com>
2022-10-07 00:47:09 +0900

    3.3章　データベースから値を取得する

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.hello;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 	
+	@Autowired
+	private HelloService service;
+	
 	@GetMapping("/hello")
 	public String getHello() {
 		//hello.htmlに画面遷移
@@ -18,14 +22,26 @@ public class HelloController {
 	}
 	
 	@PostMapping("/hello")
-	public String  postRequest(@RequestParam("text1") String str, Model model ) {
+	public String  postRequest(@RequestParam("text1") String str, Model model) {
 		
 		//画面から受け取った文字列をModelに登録
 		model.addAttribute("sample",str);
 
 		//response.htmlに画面遷移
-		return"hello/response";
+		return "hello/response";
 		
 		}
-
+	
+	@PostMapping("/hello/db")
+	public String postDbRequest (@RequestParam("text2")String id, Model model) {
+		
+		//1件検索
+		Employee employee = service.getEmployee(id);
+		
+		//検索結果をModelに登録
+		model.addAttribute("employee",employee);
+		
+		//db.htmlに画面遷移
+		return "hello/db";
+	}
 }
