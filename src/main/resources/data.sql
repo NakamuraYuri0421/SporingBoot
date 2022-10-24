f41777cec70d30bd0d96abc5b5a5295f0e0932cc
NakamuraYuri0421 <nkmr44yr@gmail.com>
2022-10-24 23:10:47 +0900

    8.2.1章　修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,26 +12,22 @@ INSERT INTO m_user(
 	,department_id
 	,role 
 )VALUES
-('system@co.jp','password','システム管理者','2000-01-01',21,1,1,'ROLE_ADMIN')
-,('user@co.jp','password','ユーザー1','2000-01-01',21,1,1,'ROLE_GENERAL')
-;
+('system@co.jp','password','システム管理者','2000-01-01',21,1,1,'ROLE_ADMIN'),
+('user@co.jp','password','ユーザー1','2000-01-01',21,1,1,'ROLE_GENERAL');
 
 /*部署マスタ*/
 INSERT INTO m_depaetment(
 	department_id
 	,department_name
 )VALUES
-(1,'システム管理者')
-,(2,'営業部')
-;
+(1,'システム管理者'),
+(2,'営業部');
 
 /*給料テーブル*/
 INSERT INTO t_salay(
 	user_id
 	,year_month
 	,salary
-)VALUES
-('user@co.jp','2020/11',280000)
-,('user@co.jp','2020/12',290000)
-,('user@co.jp','2021/01',300000)
-;
+)VALUES('user@co.jp','2020/11',280000),
+('user@co.jp','2020/12',290000),
+('user@co.jp','2021/01',300000);
