227dfcccba86f60364ef8c261364ed3d7c21ca09
NakamuraYuri0421 <nkmr44yr@gmail.com>
2022-10-26 01:35:39 +0900

    8.2.1章　修正２

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,9 @@
-INSERT INTO employee (id, name, age)
-VALUES('1', 'Tom', 30);
+INSERT INTO employee (
+	id
+	,name
+	,age
+) VALUES 
+	('1', 'Tom', 30);
 
 /*ユーザーマスタ*/
 INSERT INTO m_user(
@@ -11,23 +15,24 @@ INSERT INTO m_user(
 	,gender
 	,department_id
 	,role 
-)VALUES
-('system@co.jp','password','システム管理者','2000-01-01',21,1,1,'ROLE_ADMIN'),
-('user@co.jp','password','ユーザー1','2000-01-01',21,1,1,'ROLE_GENERAL');
+) VALUES
+	('system@co.jp','password','システム管理者','2000-01-01',21,1,1,'ROLE_ADMIN'),
+	('user@co.jp','password','ユーザー1','2000-01-01',21,1,1,'ROLE_GENERAL');
 
 /*部署マスタ*/
 INSERT INTO m_depaetment(
 	department_id
 	,department_name
-)VALUES
-(1,'システム管理者'),
-(2,'営業部');
+) VALUES
+	(1,'システム管理者'),
+	(2,'営業部');
 
 /*給料テーブル*/
 INSERT INTO t_salay(
 	user_id
 	,year_month
 	,salary
-)VALUES('user@co.jp','2020/11',280000),
-('user@co.jp','2020/12',290000),
-('user@co.jp','2021/01',300000);
+) VALUES 
+	('user@co.jp','2020/11',280000),
+	('user@co.jp','2020/12',290000),
+	('user@co.jp','2021/01',300000);
