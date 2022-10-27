a3a05cc4e75264a79447f6aa6a310de4ee6bb18e
NakamuraYuri0421 <nkmr44yr@gmail.com>
2022-10-28 01:44:51 +0900

    8.2.1章　修正３

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,38 +1,84 @@
-INSERT INTO employee (
-	id
-	,name
-	,age
-) VALUES 
-	('1', 'Tom', 30);
+INSERT INTO employee
+(
+   id,
+   name,
+   age
+)
+VALUES
+(
+   '1',
+   'Tom',
+   30
+);
+/*ユーザーマスタ*/ INSERT INTO m_user
+(
+   user_id,
+   password,
+   user_name,
+   birthday,
+   age,
+   gender,
+   department_id,
+   role
+)
+VALUES
+(
+   'system@co.jp',
+   'password',
+   'システム管理者',
+   '2000-01-01',
+   21,
+   1,
+   1,
+   'ROLE_ADMIN'
+),
 
-/*ユーザーマスタ*/
-INSERT INTO m_user(
-	user_id 
-	,password 
-	,user_name
-	,birthday 
-	,age
-	,gender
-	,department_id
-	,role 
-) VALUES
-	('system@co.jp','password','システム管理者','2000-01-01',21,1,1,'ROLE_ADMIN'),
-	('user@co.jp','password','ユーザー1','2000-01-01',21,1,1,'ROLE_GENERAL');
+(
+   'user@co.jp',
+   'password',
+   'ユーザー1',
+   '2000-01-01',
+   21,
+   1,
+   1,
+   'ROLE_GENERAL'
+);
+/*部署マスタ*/ INSERT INTO m_depaetment
+(
+   department_id,
+   department_name
+)
+VALUES
+(
+   1,
+   'システム管理者'
+),
 
-/*部署マスタ*/
-INSERT INTO m_depaetment(
-	department_id
-	,department_name
-) VALUES
-	(1,'システム管理者'),
-	(2,'営業部');
+(
+   2,
+   '営業部'
+);
+/*給料テーブル*/ INSERT INTO t_salay
+(
+   user_id,
+   year_month,
+   salary
+)
+VALUES
+(
+   'user@co.jp',
+   '2020/11',
+   280000
+),
 
-/*給料テーブル*/
-INSERT INTO t_salay(
-	user_id
-	,year_month
-	,salary
-) VALUES 
-	('user@co.jp','2020/11',280000),
-	('user@co.jp','2020/12',290000),
-	('user@co.jp','2021/01',300000);
+(
+   'user@co.jp',
+   '2020/12',
+   290000
+),
+
+(
+   'user@co.jp',
+   '2021/01',
+   300000
+);
