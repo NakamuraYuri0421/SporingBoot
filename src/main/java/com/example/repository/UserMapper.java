baa89dd5d8503fb491a86739747b2127ade24434
NakamuraYuri0421 <nkmr44yr@gmail.com>
2022-11-28 00:56:52 +0900

    Update UserMapper.java

--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -26,7 +26,7 @@ public interface UserMapper {
 	
 	/**ユーザー削除*/
 	public int deleteOne(@Param("userId")String userId);
-
-	/**ログインユーザー取得*/	
+	
+	/**ログインユーザー取得*/
 	public MUser findLoginUser(String userId);
 }
